e23d27d411a1b491ecdcd3348368de27bf11ea14
Shamseen <shamseen@users.noreply.github.com>
2022-05-17 12:24:20 -0500

    first query

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,43 @@
 import {
   ApolloClient,
-  InMemoryCache // cache of query results
+  InMemoryCache, // cache of query results,
+  gql
 } from "@apollo/client";
 
-// initialize client
+// initialize client using existing graphQL server (spaceX launches?)
 const client = new ApolloClient({
-  uri: "https://5y2ij.sse.codesandbox.io/", // existing graphQL server
+  uri: "https://n1kqy.sse.codesandbox.io/", // existing graphQL server
   cache: new InMemoryCache() // instantiate cache
 });
+
+// query
+client
+  .query({
+    query: gql`
+      query TestQuery {
+        launch(id: 56) {
+          id
+          mission {
+            name
+          }
+        }
+      }
+    `
+  })
+  .then((result) => console.log(result));
+/* - Result
+{
+  "data": {
+    "launch": {
+      "__typename": "Launch",
+      "id": "56",
+      "mission": {
+        "__typename": "Mission",
+        "name": "Paz / Starlink Demo"
+      }
+    }
+  },
+  "loading": false,
+  "networkStatus": 7
+} 
+*/
